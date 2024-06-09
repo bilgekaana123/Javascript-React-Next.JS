60d5e6af42f95633cb4189b964d91a89dbdc7bbb
bilgekaana <berberoglubilgekaan@gmail.com>
2024-06-09 23:35:34 +0300

    Javascript Course

---
+++ 01-variables-data-types/13-number-challange/script.js
@@ -0,0 +1,26 @@
+
+
+const x = Math.floor(Math.random() * 100 + 1);
+const y = Math.floor(Math.random() * 50 + 1);
+
+// Sum
+const sum = x + y;
+const sumOutput = `${x} + ${y} = ${sum}`;
+console.log(sumOutput);
+
+// Difference
+const diff = x - y;
+const diffOutput = `${x} - ${y} = ${diff}`;
+console.log(diffOutput);
+
+// Product
+const prod = x * y;
+const prodOutput = `${x} * ${y} = ${prod}`;
+console.log(prodOutput);
+
+// Difference
+const quot = x - y;
+const quotOutput = `${x} / ${y} = ${quot}`;
+console.log(diffOutput);
+
+
