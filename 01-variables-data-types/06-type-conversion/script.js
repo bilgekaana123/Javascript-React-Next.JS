50bb6b7729cf1c4237a93bfb8657927e7065b2b3
bilgekaana <berberoglubilgekaan@gmail.com>
2024-06-09 13:03:41 +0300

    Javascript Course

---
+++ 01-variables-data-types/06-type-conversion/script.js
@@ -0,0 +1,26 @@
+// String to Number
+let amount = '100';
+
+amount = parseInt(amount);
+amount = +amount;
+amount = Number(amount);
+
+// If you have a decimal
+let amount2 = 99.5;
+amount2 = parseInt(amount2);
+amount2 = parseFloat(amount2);
+
+
+// Number to String
+let amount3 = 100;
+
+amount2 = amount3.toString();
+amount2 = String();
+
+
+// Number to Bool
+let  amount4 = 1;
+amount4 = Boolean(amount4);
+
+
+
