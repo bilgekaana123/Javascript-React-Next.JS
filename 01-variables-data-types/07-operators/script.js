d682f1c6c76d9cbceb9ecd27ea49ef16fd3c23df
bilgekaana <berberoglubilgekaan@gmail.com>
2024-06-09 13:39:24 +0300

    Javascript Course

---
+++ 01-variables-data-types/07-operators/script.js
@@ -0,0 +1,39 @@
+// Arithmetic Operators
+let x;
+x = 5 + 5;
+x = 5 - 5;
+x = 5 / 5;
+x = 5 * 5;
+x = 7 % 5;
+
+// Concatenation
+x = 'Hello' + 'World';
+
+// Exponent
+x = 2 ** 3;
+
+// Increment 
+x = 1;
+x = x + 1;
+x++;
+
+// Decrement
+x = x - 1;
+x--;
+
+// Assigment Operators
+x = 10;
+x += 5;
+
+// Comparison Operators
+x = 2 == 2;     // Checks Only Value
+x = 2 === '2';  // Checks Also Type
+
+x = 2 != 2;
+x = 2 !== '2';
+
+x = 10 > 5;
+x = 10 < 11;
+x = 10 <= 5;
+x = 10 >= 5;
+console.log(x);
