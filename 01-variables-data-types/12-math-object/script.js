39078c84b4467a6973cf17c9fcd0bdda6c44f517
bilgekaana <berberoglubilgekaan@gmail.com>
2024-06-09 23:27:14 +0300

    Javascript Course

---
+++ 01-variables-data-types/12-math-object/script.js
@@ -0,0 +1,17 @@
+
+let x;
+
+x = Math.sqrt(9);
+x = Math.abs(-5);
+x = Math.round(5.7);
+x = Math.ceil(5.1);
+x = Math.floor(5.9);
+x = Math.pow(2, 3);
+x = Math.min(6,9);
+x = Math.max(6,9);
+x = Math.random();
+
+x = Math.floor(Math.random() * 10 + 1);
+
+
+console.log(x);
