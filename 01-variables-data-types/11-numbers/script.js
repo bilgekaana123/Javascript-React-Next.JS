2300017183dd4c659b64b56c4c45513383777bec
bilgekaana <berberoglubilgekaan@gmail.com>
2024-06-09 19:38:46 +0300

    Javascript Course

---
+++ 01-variables-data-types/11-numbers/script.js
@@ -0,0 +1,15 @@
+
+let x;
+const num = new Number(5.4568);
+
+x = num.toString();
+x = num.toString().length;
+x = num.toFixed(2);
+x = num.toPrecision(2);
+x = num.toExponential(2);
+x = num.toLocaleString('en-US');
+
+x = Number.MAX_VALUE;
+x = Number.MIN_VALUE;
+
+console.log(x);
