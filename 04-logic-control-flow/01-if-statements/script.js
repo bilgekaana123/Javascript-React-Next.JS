cc3894bd75845076f692bb2751c398ae548948d8
bilgekaana123 <berberoglubilgekaan@gmail.com>
2024-06-14 19:24:39 +0300

    Javascript Course

---
+++ 04-logic-control-flow/01-if-statements/script.js
@@ -0,0 +1,16 @@
+if (true) {
+   console.log('This is true');
+}
+
+const x = 10;
+const y = 5;
+
+if (x > y) {
+   console.log('X is bigger than Y');
+} else {
+   console.log('X is not bigger than Y');
+}
+
+//Shorthand If
+if (x >= y) console.log(`${x} is greater than or equal to ${y}`);
+else console.log('This is false');
