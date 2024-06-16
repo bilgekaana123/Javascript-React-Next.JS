0a30886c996c73c252a30a3e99e8124ef9c2e1bd
bilgekaana <berberoglubilgekaan@gmail.com>
2024-06-16 14:24:44 +0300

    Javascript Course

---
+++ Javascript Course/06-document-object-model/01-dom-intro/script.js
@@ -0,0 +1,16 @@
+// console.log(document);
+// console.log(document.body);
+// console.log(document.body.innerHTML);
+// console.log(document.body.innerText);
+
+console.log(document.links[0]);
+
+// document.body.innerHTML = '<h1>Hello World</h1>';
+
+// document.write('Hello from JS');
+
+// Important selector
+// const main = document.getElementById('main');
+// main.innerHTML = '<h1>Hello from main ID </h1>';
+
+document.querySelector('#main h1').innerText = 'Hello';
